36a6a80083dca73fe53219e5fb2359a997b0e374
Enes Ateş <enesates01@gmail.com>
2021-12-02 01:00:11 +0300

    create user and category table(migration)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+// use App\Traits\HasSecurePassword;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    // use HasSecurePassword;
+    
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    protected $rules = [
+        'name'     => 'required|max:255',
+        'surname'     => 'required|max:255',
+        'email'    => 'required|email|max:255|unique:users',
+        'password' => 'required|min:6|confirmed',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +27,7 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password', 'password_confirmation',
     ];
 
     /**
@@ -29,16 +36,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password_digest',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(App\Category::class);
+    }
+
 }
